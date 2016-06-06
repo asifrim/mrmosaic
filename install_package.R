aa05c0b3cf76cc777ee29ba32376bd3f449b141f
Alejandro Sifrim <as33@sanger.ac.uk>
2016-06-06 13:22:54 +0100

    Check if prereqs are installed and if not install them

--- install_package.R
+++ install_package.R
@@ -1,4 +1,9 @@
 options(echo=TRUE)
+
+list.of.packages <- c("devtools")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
 library(devtools)
 install_github("asifrim/mrmosaic",ref="development",subdir="mrmosaic")
 print("Mr.Mosaic R Package Installed...")
