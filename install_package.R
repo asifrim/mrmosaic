27d188bb846bcd96ced6c55c10f34ac181d39a79
Alejandro Sifrim <as33@sanger.ac.uk>
2016-09-15 11:25:37 +0100

    switched from dev to master depo

--- install_package.R
+++ install_package.R
@@ -5,5 +5,5 @@ new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"
 if(length(new.packages)) install.packages(new.packages,repos="https://mirrors.ebi.ac.uk/CRAN/")
 
 library(devtools)
-install_github("asifrim/mrmosaic",ref="development",subdir="mrmosaic")
+install_github("asifrim/mrmosaic",ref="master",subdir="mrmosaic")
 print("Mr.Mosaic R Package Installed...")
